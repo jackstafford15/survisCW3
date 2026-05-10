74c0cf1f53a37d664a5e155f05c162a1d99a4681
jackstafford15 <jackmstafford2222@gmail.com>
2026-05-10 23:26:08 +0100

    Add files via upload

---
+++ data/authorized_tags.js
@@ -0,0 +1,40 @@
+define({
+	authorizedTags: {
+		"subsection:foundation_and_background": {
+			"description": "subsection 1 of the literature review"
+		},
+		"subsection:early_generative_disease_progression_models": {
+			"description": "subsection 2 of the literature review"	
+		},
+		"subsection:diffusion_based_longitudinal_progression_models": {
+			"description": "subsection 3 of the literature review"
+		},
+		"subsection:modern_3_dimensional_temporally_aware_models": {
+			"description": "subsection 4 of the literature review"
+		},
+		"paper:research": {
+			"description": "a research paper"
+		},
+		"paper:survey": {
+			"description": "a survey paper"
+		},
+		"evaluation:performance": {
+			"description": "a paper using a performance-based evaluation"
+		},
+		"evaluation:application": {
+			"description": "a paper using an application-centred evaluation"
+		},
+		"model:diffusion": {
+			"description": "a diffusion-based paper"
+		},
+		"model:gan": {
+			"description": "a gan-based paper"
+		},
+		"model:counterfactual": {
+			"description": "a counterfactual-based paper"
+		},
+		"model:normalising_flow": {
+			"description": "a normalising flow-based paper"
+		}
+	}
+});
---
+++ data/generated/bib.js
@@ -0,0 +1,102 @@
+﻿define({ entries : {
+    "kazerouni2022diffusion": {
+        "author": "Kazerouni, Amirhossein and Aghdam, Ehsan Khodapanah and Heidari, Moein and Azad, Reza and Fayyaz, Mohsen and Hacihaliloglu, Ilker and Merhof, Dorit",
+        "journal": "arXiv preprint arXiv:2211.07804",
+        "keywords": "subsection:foundation_and_background, paper:survey, evaluation:performance, model:diffusion",
+        "title": "Diffusion models for medical image analysis: A comprehensive survey",
+        "type": "article",
+        "year": "2022"
+    },
+    "kim2022ddm": {
+        "author": "Kim, Boah and Ye, Jong Chul",
+        "booktitle": "MICCAI",
+        "keywords": "subsection:diffusion_based_longitudinal_progression_models, paper:research, evaluation:performance, model:diffusion",
+        "pages": "539--548",
+        "title": "Diffusion deformable model for 4D temporal medical image generation",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "kim2022diffusemorph": {
+        "author": "Kim, Boah and Han, Inhwa and Ye, Jong Chul",
+        "booktitle": "ECCV",
+        "keywords": "subsection:diffusion_based_longitudinal_progression_models, paper:research, evaluation:performance, model:diffusion",
+        "pages": "347--364",
+        "title": "DiffuseMorph: Unsupervised deformable image registration using diffusion model",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "litrico2025temporally": {
+        "author": "Litrico, Mattia and Guarnera, Francesco and Giuffrida, Mario Valerio and Ravi, Daniele and Battiato, Sebastiano",
+        "journal": "Computerized Medical Imaging and Graphics",
+        "keywords": "subsection:modern_3_dimensional_temporally_aware_models, paper:research, evaluation:performance, model:diffusion",
+        "pages": "102688",
+        "publisher": "Elsevier",
+        "title": "Temporally-aware diffusion model for brain progression modelling with bidirectional temporal regularisation",
+        "type": "article",
+        "year": "2025"
+    },
+    "pombo2023counterfactual": {
+        "author": "Pombo, Guilherme and Gray, Robert and Cardoso, M Jorge and Ourselin, Sebastien and Rees, Geraint and Ashburner, John and Nachev, Parashkev",
+        "journal": "Medical Image Analysis",
+        "keywords": "subsection:diffusion_based_longitudinal_progression_models, paper:research, evaluation:performance, model:counterfactual",
+        "pages": "102723",
+        "publisher": "Elsevier",
+        "title": "Equitable modelling of brain imaging by counterfactual augmentation with morphologically constrained 3D deep generative models",
+        "type": "article",
+        "volume": "84",
+        "year": "2023"
+    },
+    "puglisi2025brain": {
+        "author": "Puglisi, Lemuel and Alexander, Daniel C and Ravi, Daniele and Alzheimer's Disease Neuroimaging Initiative and others",
+        "journal": "Medical Image Analysis",
+        "keywords": "subsection:modern_3_dimensional_temporally_aware_models, paper:research, evaluation:performance, model:diffusion",
+        "pages": "103734",
+        "publisher": "Elsevier",
+        "title": "Brain Latent Progression: Individual-based spatiotemporal disease progression on 3D Brain MRIs via latent diffusion",
+        "type": "article",
+        "year": "2025"
+    },
+    "ravi2022degenerative": {
+        "author": "Ravi, Daniele and Blumberg, Stefano B and Ingala, Silvia and Barkhof, Frederik and Alexander, Daniel C and Oxtoby, Neil P and Alzheimer's Disease Neuroimaging Initiative and others",
+        "journal": "Medical Image Analysis",
+        "keywords": "subsection:foundation_and_background, paper:research, evaluation:performance, model:gan",
+        "pages": "102257",
+        "publisher": "Elsevier",
+        "title": "Degenerative adversarial neuroimage nets for brain scan simulations: Application in ageing and dementia",
+        "type": "article",
+        "volume": "75",
+        "year": "2022"
+    },
+    "wilms2022invertible": {
+        "author": "Wilms, Matthias and Bannister, Jordan J and Mouches, Pauline and MacDonald, M Ethan and Rajashekar, Deepthi and Langner, S{\\\"o}nke and Forkert, Nils D",
+        "journal": "IEEE Transactions on Medical Imaging",
+        "keywords": "subsection:early_generative_disease_progression_models, paper:research, evaluation:application, model:normalising_flow",
+        "number": "9",
+        "pages": "2331--2347",
+        "publisher": "IEEE",
+        "title": "Invertible modeling of bidirectional relationships in neuroimaging with normalizing flows: application to brain aging",
+        "type": "article",
+        "volume": "41",
+        "year": "2022"
+    },
+    "xia2021learning": {
+        "author": "Xia, Tian and Chartsias, Agisilaos and Wang, Chengjia and Tsaftaris, Sotirios A and Alzheimer's Disease Neuroimaging Initiative and others",
+        "journal": "Medical Image Analysis",
+        "keywords": "subsection:early_generative_disease_progression_models, paper:research, evaluation:performance, model:gan",
+        "pages": "102169",
+        "publisher": "Elsevier",
+        "title": "Learning to synthesise the ageing brain without longitudinal data",
+        "type": "article",
+        "volume": "73",
+        "year": "2021"
+    },
+    "yoon2023sadm": {
+        "author": "Yoon, Jee Seok and Zhang, Chenghao and Suk, Heung-Il and Guo, Jia and Li, Xiaoxiao",
+        "booktitle": "IPMI",
+        "keywords": "subsection:diffusion_based_longitudinal_progression_models, paper:research, evaluation:performance, model:diffusion",
+        "pages": "388--400",
+        "title": "SADM: Sequence-aware diffusion model for longitudinal medical image generation",
+        "type": "inproceedings",
+        "year": "2023"
+    }
+}});
